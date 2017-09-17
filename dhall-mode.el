3a1aaaf0b3d97102481b7b9a7b4cae6fc76555c0
Sibi Prabakaran <sibi@psibi.in>
2017-09-17 23:22:31 +0530

    Add further features

--- dhall-mode.el
+++ dhall-mode.el
@@ -1,5 +1,8 @@
 ;; -*- lexical-binding: t -*-
 
+(require 'highlight-numbers-mode)
+(require 'rainbow-delimiters)
+
 ;; Create the syntax table for this mode.
 (defvar dhall-mode-syntax-table
   (let ((st (make-syntax-table)))
@@ -15,10 +18,40 @@
     st)
   "Syntax table used while in `dhall-mode'.")
 
+;; define several category of keywords
+(setq dhall-mode-keywords '("if" "then" "else" "let" "in") )
+(setq dhall-mode-types '("Optional" "Bool" "Integer" "Double" "Text" "List" "Type"))
+(setq dhall-mode-constants '("True" "False"))
+
+(setq dhall-mode-keywords-regexp (regexp-opt dhall-mode-keywords 'words))
+(setq dhall-mode-type-regexp (regexp-opt dhall-mode-types 'words))
+(setq dhall-mode-constant-regexp (regexp-opt dhall-mode-constants 'words))
+
+(defconst dhall-mode-multiline-string-regexp
+  "''[^']*''"
+  "Regular expression for matching multiline dhall strings.")
+
+
+(defconst dhall-mode-font-lock-keywords
+  `(;; Variables
+    (,dhall-mode-type-regexp . font-lock-type-face)
+    (,dhall-mode-constant-regexp . font-lock-constant-face)
+    (,dhall-mode-keywords-regexp . font-lock-keyword-face)
+    (,dhall-mode-multiline-string-regexp . font-lock-string-face)
+    )
+  )
+
 (define-derived-mode dhall-mode prog-mode "Dhall"
   "Major mode for editing Dhall files."
   :group 'dhall
-  (setq font-lock-defaults (list nil))
+  (setq font-lock-defaults '((dhall-mode-font-lock-keywords) nil nil))
   (set (make-local-variable 'comment-start) "--")
+  (set (make-local-variable 'font-lock-multiline) t)
+  (setq-local indent-tabs-mode t)
+  (setq-local tab-width 4)
+  (highlight-numbers-mode 1)
+  (rainbow-delimiters-mode 1)
   (set-syntax-table dhall-mode-syntax-table)
   )
+
+(provide 'dhall-mode)
